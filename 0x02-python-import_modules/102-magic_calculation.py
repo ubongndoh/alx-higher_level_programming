560d08e956f5b095d086a0d550b086ebad54467f
ubongndoh <ubongndoh@gmail.com>
2022-08-25 16:54:00 -0700

    task 102

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -3,7 +3,7 @@
 
 
 def magic_calculation(a, b):
-    from calculator_1 import add, sub
+    from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
